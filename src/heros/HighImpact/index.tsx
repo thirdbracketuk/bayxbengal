f7c9bc8977a4a6addd214f03cfbdfe3d710c3239
sagarmusabbir <sagarmusabbir@gmail.com>
2025-12-28 01:40:48 +0600

    hjhjh

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -11,18 +11,15 @@ import RichText from '@/components/RichText'
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   const { setHeaderTheme } = useHeaderTheme()
 
-  useEffect(() => {
-    setHeaderTheme('dark')
-  })
+  // useEffect(() => {
+  //   setHeaderTheme('dark')
+  // })
 
   return (
-    <div
-      className="relative -mt-[10.4rem] flex items-center justify-center text-white"
-      data-theme="dark"
-    >
+    <div className="relative -mt-[10.4rem] flex items-center justify-center">
       <div className="container mb-8 z-10 relative flex items-center justify-center">
         <div className="max-w-[36.5rem] md:text-center">
-          {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+          {richText && <RichText className="mb-6 " data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex md:justify-center gap-4">
               {links.map(({ link }, i) => {
